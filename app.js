0f7f0b59ff17a8137cb228d1cc1125099d24d641
Enrico Zappalà <zpp.enrico@gmail.com>
2022-02-25 09:40:22 +0100

    Check CLI argument

--- app.js
+++ app.js
@@ -3,11 +3,26 @@ const fs = require('fs');
 const sharp = require('sharp');
 const { program } = require('commander');
 
+program
+    .option('-s, --size <integer>', 'set desired size in KB, max allowed value is 2048')
+
+program.parse();
+
+const options = program.opts();
+const requestedSize = Number.parseInt(options.size);
+
+// Check if size parameter exists and if it's a number
+if (requestedSize)
+    if (requestedSize <= 2048)
+        console.log('\x1b[35m%s\x1b[0m', '\nOutput desired size =>', `~ ${requestedSize} KB\n`);
+    else
+        return console.log('\x1b[31m%s\x1b[0m', `\nError: max allowed KB value is 2048"\n`);
+else
+    return console.log('\x1b[31m%s\x1b[0m', `\nError: you must provide a size in KB. Example: node app.js -s 500"\n`);
+
 // Get images path
 const sourcePath = path.join(__dirname, 'input');
 const destinationPath = path.join(__dirname, 'output');
-
-console.log(sourcePath, destinationPath);
 
 //TODO: catch size parameter
 
